512426840b037caae2b9788fa21335c9cdd048d2
Steve Padnos <spadnos@gmail.com>
2025-10-24 16:07:53 -0700

    redirect

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -71,10 +71,6 @@ export async function signInWithGoogle() {
     provider: 'google',
     options: {
       redirectTo,
-      queryParams: {
-        access_type: 'offline',
-        prompt: 'consent',
-      },
     },
   });
 
